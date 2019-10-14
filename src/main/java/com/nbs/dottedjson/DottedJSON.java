06518fba2bc82e84adfdf7887be831740ffd1af1
Nick <nick@networkbasedsolutions.com>
2019-10-13 21:08:04 -0700

    New - Initial Project Structure and Main Class

---
+++ src/main/java/com/nbs/dottedjson/DottedJSON.java
@@ -0,0 +1,46 @@
+package com.nbs.dottedjson;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class DottedJSON {
+    private List<String> paths;
+
+    public DottedJSON(List<String> paths) {
+        this.paths = paths;
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject root = new JSONObject();
+        for (String currentPath : paths) {
+            String[] nodes = currentPath.split("\\.");
+
+            JSONObject parent = root;
+            for (String node : nodes) {
+                String[] pair = node.split(":", 2);
+                String key = pair[0];
+
+                if (parent.has(key)) {
+                    parent = parent.getJSONObject(key);
+                } else {
+                    JSONObject child = new JSONObject();
+                    if (pair.length > 1) {
+                        parent.put(key, pair[1]);
+                    } else {
+                        parent.put(key, child);
+                    }
+
+                    parent = child;
+                }
+            }
+        }
+
+        return root;
+    }
+
+    @Override
+    public String toString() {
+        return toJSONObject().toString();
+    }
+}
---
+++ src/test/java/com/nbs/dottedjson/DottedJSONTest.java
@@ -0,0 +1,79 @@
+package com.nbs.dottedjson;
+
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DottedJSONTest {
+    @Test
+    public void testTestToString() {
+        List<String> paths = Arrays.asList(
+            "person.firstName:Sally",
+            "person.lastName:Smith",
+            "person.age:42",
+            "person.address.street1:1234 Test Ave",
+            "person.address.street2:Appt 123",
+            "person.address.city:San Diego",
+            "person.address.state:CA",
+            "comments:This is extra info :} Hurray!"
+        );
+
+        String expected = ""
+            + "{\n"
+            + "  \"comments\": \"This is extra info :} Hurray!\",\n"
+            + "  \"person\": {\n"
+            + "    \"firstName\": \"Sally\",\n"
+            + "    \"lastName\": \"Smith\",\n"
+            + "    \"address\": {\n"
+            + "      \"city\": \"San Diego\",\n"
+            + "      \"street1\": \"1234 Test Ave\",\n"
+            + "      \"street2\": \"Appt 123\",\n"
+            + "      \"state\": \"CA\"\n"
+            + "    },\n"
+            + "    \"age\": \"42\"\n"
+            + "  }\n"
+            + "}";
+
+        DottedJSON dottedJSON = new DottedJSON(paths);
+        assertThat(dottedJSON.toString()).isEqualTo(new JSONObject(expected).toString());
+    }
+
+    @Test
+    public void testToJSONObject() {
+        List<String> paths = Arrays.asList(
+            "person.firstName:Sally",
+            "person.lastName:Smith",
+            "person.age:42",
+            "person.address.street1:1234 Test Ave",
+            "person.address.street2:Appt 123",
+            "person.address.city:San Diego",
+            "person.address.state:CA",
+            "comments:This is extra info :} Hurray!"
+        );
+
+        String expected = ""
+            + "{\n"
+            + "  \"comments\": \"This is extra info :} Hurray!\",\n"
+            + "  \"person\": {\n"
+            + "    \"firstName\": \"Sally\",\n"
+            + "    \"lastName\": \"Smith\",\n"
+            + "    \"address\": {\n"
+            + "      \"city\": \"San Diego\",\n"
+            + "      \"street1\": \"1234 Test Ave\",\n"
+            + "      \"street2\": \"Appt 123\",\n"
+            + "      \"state\": \"CA\"\n"
+            + "    },\n"
+            + "    \"age\": \"42\"\n"
+            + "  }\n"
+            + "}";
+
+        DottedJSON dottedJSON = new DottedJSON(paths);
+        assertThat(dottedJSON.toJSONObject())
+            .usingRecursiveComparison()
+            .isEqualTo(new JSONObject(expected));
+    }
+}
