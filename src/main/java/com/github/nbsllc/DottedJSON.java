ee04a8bbc04a45e6362f76e45ae5d69b72cf3739
Nick <nick@networkbasedsolutions.com>
2019-10-14 23:01:30 -0700

    New - Handle Data Types
    * Data types can be used as values: Integer(123), Float(123.4), Boolean(true), etc.
    * Also fixed a bug where values containing periods were causing issues.

--- src/main/java/com/github/nbsllc/DottedJSON.java
+++ src/main/java/com/github/nbsllc/DottedJSON.java
@@ -2,6 +2,7 @@ package com.github.nbsllc;
 
 import org.json.JSONObject;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,23 +17,21 @@ public class DottedJSON {
     public JSONObject toJSONObject() {
         JSONObject root = new JSONObject();
         for (String currentPath : paths) {
-            String[] nodes = currentPath.split("\\.");
+            String[] pair = currentPath.split(":", 2);
+            String[] nodes = pair[0].split("\\.");
 
             JSONObject parent = root;
-            for (String node : nodes) {
-                String[] pair = node.split(":", 2);
-                String key = pair[0];
-
-                if (parent.has(key)) {
-                    parent = parent.getJSONObject(key);
+            for (int i = 0; i < nodes.length; i++) {
+                String node = nodes[i];
+                if (parent.has(node)) {
+                    parent = parent.getJSONObject(node);
                 } else {
                     JSONObject child = new JSONObject();
-                    if (pair.length > 1) {
-                        parent.put(key, parseValue(pair[1]));
+                    if (i == nodes.length - 1) {
+                        parent.put(node, parseValue(pair[1]));
                     } else {
-                        parent.put(key, child);
+                        parent.put(node, child);
                     }
-
                     parent = child;
                 }
             }
@@ -47,10 +46,17 @@ public class DottedJSON {
     }
 
     private Object parseValue(String value) {
-        Pattern integerPattern = Pattern.compile("Integer\\((.+)\\)");
-        Matcher matcher = integerPattern.matcher(value);
+        Pattern dataTypePattern = Pattern.compile("^([^(]+)\\(([^)]+)\\)$");
+        Matcher matcher = dataTypePattern.matcher(value);
         if (matcher.find()) {
-            return Integer.valueOf(matcher.group(1));
+            try {
+                Class<?> dataType = Class.forName("java.lang." + matcher.group(1));
+                Method valueOf = dataType.getMethod("valueOf", String.class);
+                return valueOf.invoke(null, matcher.group(2));
+            } catch (Exception e) {
+                String error = String.format("Unable to parse data type: %s.", value);
+                throw new RuntimeException(error, e);
+            }
         }
 
         return value;
