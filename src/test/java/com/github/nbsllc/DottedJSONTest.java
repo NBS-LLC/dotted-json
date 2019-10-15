4d0444f217077db907d49904cfb87225b2683e28
Network Based Solutions, LLC <35822984+NBS-LLC@users.noreply.github.com>
2019-10-14 23:04:01 -0700

    New - Handle Data Types (#2)
    * Data types can be used as values: Integer(123), Float(123.4), Boolean(true), etc.
    * Also fixed a bug where values containing periods were causing issues.

--- src/test/java/com/github/nbsllc/DottedJSONTest.java
+++ src/test/java/com/github/nbsllc/DottedJSONTest.java
@@ -4,17 +4,73 @@ import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class DottedJSONTest {
+    @Test
+    public void testBooleanDataType() {
+        List<String> paths = Collections.singletonList(
+            "married:Boolean(true)"
+        );
+
+        DottedJSON dottedJSON = new DottedJSON(paths);
+        assertThat(dottedJSON.toJSONObject().getBoolean("married")).isTrue();
+    }
+
+    @Test
+    public void testFloatDataType() {
+        List<String> paths = Collections.singletonList(
+            "height:Float(5.667)"
+        );
+
+        DottedJSON dottedJSON = new DottedJSON(paths);
+        assertThat(dottedJSON.toJSONObject().getFloat("height")).isEqualTo(5.667f);
+    }
+
+    @Test
+    public void testImproperValueDataType() {
+        List<String> paths = Collections.singletonList(
+            "height:Integer(5.667)"
+        );
+
+        DottedJSON dottedJSON = new DottedJSON(paths);
+        assertThatThrownBy(dottedJSON::toJSONObject)
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("Unable to parse data type: Integer(5.667).");
+    }
+
+    @Test
+    public void testIntegerDataType() {
+        List<String> paths = Collections.singletonList(
+            "age:Integer(42)"
+        );
+
+        DottedJSON dottedJSON = new DottedJSON(paths);
+        assertThat(dottedJSON.toJSONObject().getInt("age")).isEqualTo(42);
+    }
+
+    @Test
+    public void testInvalidDataType() {
+        List<String> paths = Collections.singletonList(
+            "height:Invalid(5.667)"
+        );
+
+        DottedJSON dottedJSON = new DottedJSON(paths);
+        assertThatThrownBy(dottedJSON::toJSONObject)
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("Unable to parse data type: Invalid(5.667).");
+    }
+
     @Test
     public void testTestToString() {
         List<String> paths = Arrays.asList(
             "person.firstName:Sally",
             "person.lastName:Smith",
-            "person.age:42",
+            "person.age:Integer(42)",
             "person.address.street1:1234 Test Ave",
             "person.address.street2:Appt 123",
             "person.address.city:San Diego",
@@ -34,7 +90,7 @@ public class DottedJSONTest {
             + "      \"street2\": \"Appt 123\",\n"
             + "      \"state\": \"CA\"\n"
             + "    },\n"
-            + "    \"age\": \"42\"\n"
+            + "    \"age\": 42\n"
             + "  }\n"
             + "}";
 
@@ -43,11 +99,25 @@ public class DottedJSONTest {
     }
 
     @Test
+    public void testThatValuesCanContainPeriods() {
+        List<String> paths = Collections.singletonList(
+            "extra.data.comment:This sentence has two periods. Yep two."
+        );
+
+        String expected = "{\"extra\":{\"data\":{\"comment\":\"This sentence has two periods. Yep two.\"}}}";
+
+        DottedJSON dottedJSON = new DottedJSON(paths);
+        assertThat(dottedJSON.toJSONObject())
+            .usingRecursiveComparison()
+            .isEqualTo(new JSONObject(expected));
+    }
+
+    @Test
     public void testToJSONObject() {
         List<String> paths = Arrays.asList(
             "person.firstName:Sally",
             "person.lastName:Smith",
-            "person.age:42",
+            "person.age:Integer(42)",
             "person.address.street1:1234 Test Ave",
             "person.address.street2:Appt 123",
             "person.address.city:San Diego",
@@ -67,7 +137,7 @@ public class DottedJSONTest {
             + "      \"street2\": \"Appt 123\",\n"
             + "      \"state\": \"CA\"\n"
             + "    },\n"
-            + "    \"age\": \"42\"\n"
+            + "    \"age\": 42\n"
             + "  }\n"
             + "}";
 
